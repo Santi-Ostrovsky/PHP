e4ad2eb79a23cee0fefb367fd0c6a222fbd3eb74
Teclab1 <s.ostrovsky@hotmail.com>
2023-07-01 12:17:26 -0300

    update

---
+++ 01.php
@@ -0,0 +1,13 @@
+<!-- 
+
+SIEMPRE TERMINAR LAS LINEAS CON ';'
+
+DECLARACIÓN DE VARIABLES --→ $
+
+TIPOS DE DATOS:
+    - String (cadena de texto)
+    - Number (número - integer & decimal/float)
+    - Boolean (booleano - true/false)
+    - Null (valor nulo)
+
+ -->
